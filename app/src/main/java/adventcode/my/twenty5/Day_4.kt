ad15ae01af9ce953b7c2483f9d98f5ee27169170
Chandra <b.chandrasaimohan@gmail.com>
2025-12-04 22:36:59 +0530

    Day 4 solutions.

--- app/src/main/java/adventcode/my/twenty5/Day_4.kt
+++ app/src/main/java/adventcode/my/twenty5/Day_4.kt
@@ -1,5 +1,8 @@
 package adventcode.my.twenty5
 
+import adventcode.my.twenty5.utils.printAdjacent
+import adventcode.my.twenty5.utils.printAdjacentForFilteredCharacter
+import adventcode.my.twenty5.utils.printAdjacentForFilteredCharacterAndReturnUpdatedList
 import adventcode.my.twenty5.utils.readLinesAsList
 
 /*
@@ -19,70 +22,35 @@ fun day_4_part1(){
  */
 fun main() {
     println("Hello, world!!!")
-    val grid = listOf(
-        "..@@.@@@@.",
-        "@@@.@.@.@@",
-        "@@@@@.@.@@",
-        "@.@@@@..@.",
-        "@@.@@@@.@@",
-        ".@@@@@@@.@",
-        ".@.@.@.@@@",
-        "@.@@@.@@@@",
-        ".@@@@@@@@.",
-        "@.@.@@@.@."
-    )
 
-    printAdjacent(grid)
+    val input = readLinesAsList("day4_input_test.txt")
+    println("Received Input list is $input")
+    day4_part1(input)
+    day4_part2(input)
 }
 
-fun printAdjacent(grid: List<String>) {
+fun day4_part1(inputList:List<String>){
+    val result = printAdjacentForFilteredCharacter(inputList)
+    println("Received Result for Part1 is $result")
+    println("**********************************************************")
+    println("**********************************************************")
+}
 
-    // 8 surrounding directions
-    val directions = listOf(
-        -1 to -1,   // top-left
-        -1 to  0,   // top
-        -1 to  1,   // top-right
-        0 to -1,   // left
-        0 to  1,   // right
-        1 to -1,   // bottom-left
-        1 to  0,   // bottom
-        1 to  1    // bottom-right
-    )
+fun day4_part2(inputList:List<String>){
+    println("**********************************************************")
+    var finalResult = 0
+    var tempMutableList = inputList
+    var tempCount: Int
+    do {
+        val (count, newGrid) = printAdjacentForFilteredCharacterAndReturnUpdatedList(tempMutableList)
+        tempCount = count
+        tempMutableList = newGrid.toMutableList()
+        finalResult += tempCount
 
-    val rows = grid.size
-    val cols = grid[0].length
-    var globalCount = 0
-    var localCount = 0
+    }while(tempCount > 0)
 
-    for (r in 0 until rows) {
-        for (c in 0 until cols) {
-            localCount = 0
 
-            if(grid[r][c] == '@') {
-                // println("Cell ($r,$c) = '${grid[r][c]}'")
-                for ((dr, dc) in directions) {
-                    val nr = r + dr
-                    val nc = c + dc
+  println("Final Result  for part2 is $finalResult")
 
-                    if (nr in 0 until rows && nc in 0 until cols) {
-
-                        if(grid[nr][nc] == '@'){
-                            println("Neighbor at ($nr,$nc) = '${grid[nr][nc]}'")
-                            localCount += 1
-                        }
-                    } else {
-                        println("    Neighbor at ($nr,$nc) = OUT OF GRID")
-                    }
-                }
-                println("LocalCount for ${grid[r][c]} is ${localCount} and $r$c]")
-                if(localCount  < 4 ){
-                    globalCount += 1
-                }
-            }
-            println("-------------------------------- ")
-        }
-
-    }
-    println("GlobalCount is $globalCount")
 
 }
--- app/src/main/java/adventcode/my/twenty5/utils/ReadInput.kt
+++ app/src/main/java/adventcode/my/twenty5/utils/ReadInput.kt
@@ -184,3 +184,102 @@ fun printAdjacent(grid: List<String>) {
     }
 }
 
+fun printAdjacentForFilteredCharacter(grid:List<String>,filteredCharacter:Char = '@') : Int {
+
+
+    val directions = listOf(
+        -1 to -1,   // top-left
+        -1 to  0,   // top
+        -1 to  1,   // top-right
+        0 to -1,   // left
+        0 to  1,   // right
+        1 to -1,   // bottom-left
+        1 to  0,   // bottom
+        1 to  1    // bottom-right
+    )
+
+    val rows = grid.size
+    val cols = grid[0].length
+    var globalCount = 0
+    var localCount = 0
+
+    for (r in 0 until rows) {
+        for (c in 0 until cols) {
+            localCount = 0
+
+            if(grid[r][c] == filteredCharacter) {
+                // println("Cell ($r,$c) = '${grid[r][c]}'")
+                for ((dr, dc) in directions) {
+                    val nr = r + dr
+                    val nc = c + dc
+
+                    if (nr in 0 until rows && nc in 0 until cols) {
+
+                        if(grid[nr][nc] == filteredCharacter){
+                            println("Neighbor at ($nr,$nc) = '${grid[nr][nc]}'")
+                            localCount += 1
+                        }
+                    } else {
+                        println("    Neighbor at ($nr,$nc) = OUT OF GRID")
+                    }
+                }
+                println("LocalCount for ${grid[r][c]} is ${localCount} and $r$c]")
+                if(localCount  < 4 ){
+                    globalCount += 1
+                }
+            }
+            println("-------------------------------- ")
+        }
+
+    }
+    println("GlobalCount is $globalCount")
+    return  globalCount
+}
+
+fun printAdjacentForFilteredCharacterAndReturnUpdatedList(grid:List<String>,filteredCharacter:Char = '@') : Pair<Int, List<String>> {
+
+    val directions = listOf(
+        -1 to -1, -1 to 0, -1 to 1,
+        0 to -1,          0 to 1,
+        1 to -1,  1 to 0,  1 to 1
+    )
+
+    val rows = grid.size
+  //  if (rows == 0) return grid
+    val cols = grid[0].length
+
+    // Create a read-only char view of the original grid (we will never mutate this)
+    val original = grid.map { it.toCharArray() }
+
+    // Create a mutable copy that we will update and eventually return
+    val updated = grid.map { it.toCharArray() }.toMutableList()
+
+    var globalCount = 0
+
+    for (r in 0 until rows) {
+        for (c in 0 until cols) {
+            if (original[r][c] != '@') continue
+
+            var localCount = 0
+            for ((dr, dc) in directions) {
+                val nr = r + dr
+                val nc = c + dc
+                if (nr in 0 until rows && nc in 0 until cols) {
+                    if (original[nr][nc] == '@') localCount++
+                }
+            }
+
+            if (localCount < 4) {
+                globalCount++
+                // WRITE into the separate mutable copy only
+                updated[r][c] = 'x'
+            }
+        }
+    }
+
+    // Convert mutable char arrays back to List<String> to return
+    val result = updated.map { String(it) }
+    println("GlobalCount is $globalCount")
+    println("result")
+    return Pair(globalCount, result)
+}
